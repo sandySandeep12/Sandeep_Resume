c9ad246295edae34f8d66711286756888de2934d
Bhukya sandeep 001 <157693998+sandySandeep12@users.noreply.github.com>
2025-01-19 14:40:43 +0530

    Add files via upload

---
+++ javascripte/main.js
@@ -0,0 +1,5 @@
+var typed = new Typed(".auto-type",{
+    strings : [ "UI/UX Designer","User Research","Forntend Developer", "Web Developer","Logo Designer " ,"Graphi Designer " ],
+    typespeed : 5000,
+    loop : 500
+})
